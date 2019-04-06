965c66e60c1f9f23fb0cffa1ce6f644febce8935
huasha.zhao <creislerzhao@gmail.com>
2019-04-06 10:21:42 -0700

    add some scripts

--- scripts/sync_data.sh
+++ scripts/sync_data.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+PROJ_PATH=..
+cd $PROJ_PATH
+
+rsync -razv recognition-crnn/model/zh_model huasha@hb:ocr_dev/recognition-crnn/model/
+rsync -razv detection-ctpn/checkpoints_mlt huasha@hb:ocr_dev/detection-ctpn/
