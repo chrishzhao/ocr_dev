256091711dd55ddb3fd898a3735314a3ea4e37d6
huasha.zhao <creislerzhao@gmail.com>
2019-04-06 13:13:31 -0700

    rename package

--- scripts/sync_data.sh
+++ scripts/sync_data.sh
@@ -3,5 +3,5 @@
 PROJ_PATH=..
 cd $PROJ_PATH
 
-rsync -razv recognition-crnn/model/zh_model huasha@hb:ocr_dev/recognition-crnn/model/
+#rsync -razv recognition-crnn/model/zh_model huasha@hb:ocr_dev/recognition-crnn/model/
 rsync -razv detection-ctpn/checkpoints_mlt huasha@hb:ocr_dev/detection-ctpn/
