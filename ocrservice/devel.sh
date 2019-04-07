a2bf98f28a18e7b17f0f66d8e4ca81626f925428
huasha.zhao <creislerzhao@gmail.com>
2019-04-07 13:54:22 -0700

    update deploy script

--- ocrservice/devel.sh
+++ ocrservice/devel.sh
@@ -2,8 +2,9 @@
 
 start() {
     export FLASK_APP=ocrservice.py
-    export FLASK_DEBUG=1
-    python -m flask run
+    #export FLASK_DEBUG=1
+    python -m flask run --host=0.0.0.0
+    #python -m flask run
 }
 
 $1 $2 $3 $4 $5
